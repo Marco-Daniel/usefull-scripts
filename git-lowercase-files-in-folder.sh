3077de1795a825e4b6a34a34b04c172f9aed1a9c
Marco-Daniel <48966669+Marco-Daniel@users.noreply.github.com>
2023-08-15 12:04:16 +0200

    Update git-lowercase-files-in-folder.sh

--- git-lowercase-files-in-folder.sh
+++ git-lowercase-files-in-folder.sh
@@ -1,1 +1,5 @@
+# all letters
 for f in *.ts; do git mv $f $(echo $f | tr '[:upper:]' '[:lower:']); done 
+
+# only first
+for f in *.ts; do git mv $f "$(echo ${f:0:1} | tr '[:upper:]' '[:lower:]')${f:1}"; done
